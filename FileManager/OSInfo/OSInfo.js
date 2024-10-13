5ee0ef369df7b2a91fcfab743ba3e6d1d6f7d2ac
Sergey <seleznev-krista@mail.ru>
2024-10-14 02:11:25 +0300

    refactor: OSInfo

--- FileManager/OSInfo/OSInfo.js
+++ FileManager/OSInfo/OSInfo.js
@@ -1,66 +1,57 @@
-import os from 'os'
+import os from 'os';
 import { EventEmitter } from '../eventEmitter/eventEmitter.js';
 import { OutputHandler } from '../outputHandler/outputHandler.js';
 
-export class OSInfo{
-
+export class OSInfo {
     #prefix = '--';
-    #errorText = 'the key for the "os" command is incorrect'
-    #commands = {
-        'eol': this.outputEOL,
-        'cpus': this.outputCpus,
-        'homedir': this.outputHomedir,
-        'username' : this.outputUsername,
-        'architecture': this.outputArchitecture
-    }
+    #errorText = 'the key for the "os" command is incorrect';
     #outputStartMessage = {
-      EOL: 'EOL:',
-      cpus: 'total cpus:',
-      homedir: 'Homedir:',
-      username: 'Username:',
-      architecture: 'Architecture:'
+        EOL: 'EOL:',
+        cpus: 'total cpus:',
+        homedir: 'Homedir:',
+        username: 'Username:',
+        architecture: 'Architecture:'
+    };
+
+    #commands = {
+        eol: () => this.outputResult('EOL', JSON.stringify(os.EOL)),
+        cpus: () => this.outputCpus(),
+        homedir: () => this.outputResult('homedir', os.homedir()),
+        username: () => this.outputResult('username', os.userInfo().username),
+        architecture: () => this.outputResult('architecture', os.arch())
+    };
+
+    #eventEmitter = new EventEmitter();
+
+    constructor() {
+        this.#eventEmitter.on(this.#eventEmitter.events.os, this.executeCommand);
     }
 
-    #eventEmitter = new EventEmitter()
-
-    constructor(){
-        this.#eventEmitter.on(this.#eventEmitter.events.os, this.executeСommand)
-    }
-    
-     executeСommand = (currentCommand) => {
+    executeCommand = (currentCommand) => {
         currentCommand = String(currentCommand).trim().slice(this.#prefix.length).toLowerCase();
-        try{
-         this.#commands[currentCommand].call(this)
-        }
-        catch (error){
-          OutputHandler.showInputError(this.#errorText)
-        }
-        OutputHandler.showCurrentDir();       
-     }
-     
-     outputEOL ()  {
-        OutputHandler.showResult(`${this.#outputStartMessage.EOL} ${JSON.stringify(os.EOL)}`)
-     }
+        const command = this.#commands[currentCommand];
 
-     outputCpus () {
-        const MHzInGHz = 1000
-        const cpusInfo = os.cpus().map((item) => {
-            return { model: item.model, speed: `${item.speed/MHzInGHz} GHz`}
-       })       
-       console.table(cpusInfo)   
-       OutputHandler.showResult(`${this.#outputStartMessage.cpus} ${os.cpus().length}`)    
-     }
+        if (command) {
+            command();
+        } else {
+            OutputHandler.showInputError(this.#errorText);
+        }
 
-     outputHomedir () {
-      OutputHandler.showResult(`${this.#outputStartMessage.homedir} ${os.homedir()}`) 
-     }
-     
-     outputUsername ()  {
-      OutputHandler.showResult(`${this.#outputStartMessage.username} ${os.userInfo().username}`) 
-     }
-     
-     outputArchitecture()  {      
-       OutputHandler.showResult(`${this.#outputStartMessage.architecture} ${os.arch()}`) 
-     }
- }
- 
+        OutputHandler.showCurrentDir();
+    };
+
+    outputResult(key, value) {
+        OutputHandler.showResult(`${this.#outputStartMessage[key]} ${value}`);
+    }
+
+    outputCpus() {
+        const MHzInGHz = 1000;
+        const cpusInfo = os.cpus().map(({ model, speed }) => ({
+            model,
+            speed: `${speed / MHzInGHz} GHz`
+        }));
+
+        console.table(cpusInfo);
+        OutputHandler.showResult(`${this.#outputStartMessage.cpus} ${os.cpus().length}`);
+    }
+}
